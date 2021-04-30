7b238672f2328ee03649664b2321ad1921a95595
alyssa-adams <amadam15@asu.edu>
2021-04-30 13:38:42 -0500

    Better plots

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import csv
 import re
+import scipy
+import copy
+import numpy as np
+import pandas as pd
 import networkx as nx
 from networkx.algorithms import bipartite
+
+from sklearn import datasets, linear_model
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from scipy import stats
+
+import seaborn as sns
 import matplotlib.pyplot as plt
 
+from pybdm import BDM
+import pybdm
+pybdm.options.set(raise_if_zero=False)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    # -------------- Load in data ----------------
 
     # read in network plume file
     reaction_data = {}
@@ -55,7 +66,9 @@ if __name__ == '__main__':
     plumes = ['guaymas_basin', 'cayman_von_damm', 'cayman_picard', 'lau_basin_abe', 'lau_basin_kilo_moana', 'lau_basin_mariner']
     temps = ['4.9C', '2.95C']
 
+    # enter the node attribute data for each
     plume_data = {}
+    plume_data_percent_yield = {}
 
     plume_data['guaymas_basin'] = {
         '4.9C': {
@@ -171,7 +184,7 @@ if __name__ == '__main__':
             'R-Mn-1': 1.596469403
         },
         '2.95C': {
-            'total_energy': 12.23,
+            'total_energy': 12.23,  # size of node, but size of node could also be proportion of reaction
             'R-H-1': 0.114585213,
             'R-M-1': 0.083011873,
             'R-S-1': 9.717413859,
@@ -180,6 +193,120 @@ if __name__ == '__main__':
             'R-Mn-1': 0.58227373
         },
     }
+
+    plume_data_percent_yield['guaymas_basin'] = {
+        '4.9C': {
+            'R-H-1': 21.2,
+            'R-M-1': 2.1,
+            'R-S-1': 0.0,
+            'R-S-2': 76.6,
+            'R-I-2': 0.2,
+            'N/A': 0.0
+        },
+        '2.95C': {
+            'R-H-1': 1.3,
+            'R-M-1': 92.2,
+            'R-S-1': 6.4,
+            'R-S-2': 0.0,
+            'R-I-2': 0.0,
+            'N/A': 0.0
+        },
+    }
+
+    plume_data_percent_yield['cayman_von_damm'] = {
+        '4.9C': {
+            'R-H-1': 50.6,
+            'R-M-1': 25.9,
+            'R-S-1': 16.3,
+            'R-S-2': 0.8,
+            'R-A-1': 5.9,
+            'N/A': 0.6
+        },
+        '2.95C': {
+            'R-H-1': 51.1,
+            'R-M-1': 26.4,
+            'R-S-1': 21.8,
+            'R-S-2': 0.0,
+            'R-A-1': 0.0,
+            'N/A': 0.7
+        },
+    }
+
+    plume_data_percent_yield['cayman_picard'] = {
+        '4.9C': {
+            'R-H-1': 39.1,
+            'R-M-1': 0.8,
+            'R-S-1': 57.6,
+            'R-A-1': 0.4,
+            'R-I-1': 2.1,
+            'N/A': 0.0
+        },
+        '2.95C': {
+            'R-H-1': 38.8,
+            'R-M-1': 0.8,
+            'R-S-1': 57.9,
+            'R-A-1': 0.5,
+            'R-I-1': 2.1,
+            'N/A': 0.0
+        },
+    }
+
+    plume_data_percent_yield['lau_basin_abe'] = {
+        '4.9C': {
+            'R-H-1': 1.9,
+            'R-M-1': 1.4,
+            'R-S-1': 92.7,
+            'R-A-1': 2.6,
+            'N/A': 1.3
+        },
+        '2.95C': {
+            'R-H-1': 1.9,
+            'R-M-1': 1.4,
+            'R-S-1': 92.8,
+            'R-A-1': 2.6,
+            'N/A': 1.3
+        },
+    }
+
+    plume_data_percent_yield['lau_basin_kilo_moana'] = {
+        '4.9C': {
+            'R-H-1': 2.8,
+            'R-M-1': 1.2,
+            'R-S-1': 90.8,
+            'R-A-1': 1.5,
+            'R-I-1': 3.3,
+            'N/A': 0.4
+        },
+        '2.95C': {
+            'R-H-1': 2.8,
+            'R-M-1': 1.2,
+            'R-S-1': 90.9,
+            'R-A-1': 1.5,
+            'R-I-1': 3.2,
+            'N/A': 0.4
+        },
+    }
+
+    plume_data_percent_yield['lau_basin_mariner'] = {
+        '4.9C': {
+            'R-H-1': 1.0,
+            'R-M-1': 0.7,
+            'R-S-1': 79.7,
+            'R-A-1': 1.3,
+            'R-I-1': 13.1,
+            'R-Mn-1': 4.3
+        },
+        '2.95C': {
+            'R-H-1': 0.9,
+            'R-M-1': 0.7,
+            'R-S-1': 79.5,
+            'R-A-1': 1.3,
+            'R-I-1': 12.9,
+            'R-Mn-1': 4.8
+        },
+    }
+
+    # -------------- Make into networks ----------------
 
     # turn these into a directed bipartite network with edge attributes
     # bipartite graph
@@ -194,7 +321,7 @@ if __name__ == '__main__':
 
         for temp in temps:
 
-            plume_reaction_energies = plume_data[plume][temp]
+            plume_reaction_energies = plume_data_percent_yield[plume][temp]
 
             for reaction_id in plume_reaction_energies.keys():
 
@@ -205,6 +332,7 @@ if __name__ == '__main__':
                 electrons = reaction_data_part['electrons']
                 dg_1 = reaction_data_part['dg_1']
                 dg_25 = reaction_data_part['dg_25']
+                dg_100 = reaction_data_part['dg_100']
 
                 # bipartite graph with reactions as one node color
                 G.add_node(reaction_id, bipartite=1, electrons=electrons, dg_1=dg_1, dg_25=dg_25, dg_100=dg_100, energy=plume_reaction_energies[reaction_id])
@@ -241,7 +369,104 @@ if __name__ == '__main__':
 
             plume_networks[plume+'_'+temp] = G
 
-    # plot the networks
+    # apply BDM measure to each graph
+    # perturb each reaction node to see the change in complexity
+    # save that value as a node attribute
+
+    bdm = BDM(ndim=2)
+
+    for plume_network in plume_networks.keys():
+
+        # get bdm for graph
+        G = plume_networks[plume_network]
+        bottom_nodes, top_nodes = bipartite.sets(G)
+
+        adj = nx.adj_matrix(G)
+        adj = scipy.sparse.csr_matrix.toarray(adj)
+        bdm_before = bdm.bdm(adj)
+
+        for reaction_node in bottom_nodes:
+
+            # get bdm for graph with removed node
+            removed_graph = copy.deepcopy(G)
+            removed_graph.remove_node(reaction_node)
+            adj = nx.adj_matrix(removed_graph)
+            adj = scipy.sparse.csr_matrix.toarray(adj)
+            bdm_after = bdm.bdm(adj)
+
+            # add difference in node attribute
+            G.nodes[reaction_node]['dC'] = bdm_before - bdm_after
+            G.nodes[reaction_node]['%dC'] = (bdm_before - bdm_after)/bdm_before
+
+            # add network structure measures here
+            G.nodes[reaction_node]['degree'] = list(G.degree([reaction_node]))[0][1]
+
+    # -------------- Plot results ----------------
+
+    # scatterplot of complexity vs. one of (degree, electrons, dg_1, dg_25, dg_100, energy)
+    # get node attributes into a df
+
+    node_data = []
+
+    for network in plume_networks.keys():
+
+        node_dict = plume_networks[network].nodes._nodes
+        node_dict2 = {}
+        for node in node_dict.keys():
+            nd = node_dict[node]
+            if nd['bipartite'] == 1:
+                nd['Reaction ID'] = node
+                node_dict2[node] = nd
+
+        # make into a df to concat together with others
+        node_df = pd.DataFrame.from_dict(node_dict2, orient='index')
+        plume = re.findall('[^\d]+', network)[0][:-1]
+        temp = network.split('_')[-1]
+        node_df['Plume'] = plume
+        node_df['Sample Temp'] = temp
+        node_data.append(node_df)
+
+    node_data = pd.concat(node_data)
+
+    # scatterplots
+    sns.set(rc={'figure.figsize': (10, 7)})
+    sns.set(font_scale=1.5)
+    scatter = sns.scatterplot(data=node_data, x="dC", y="energy", hue="Plume", style="Reaction ID", s=100, linewidth=0)
+    #scatter = sns.scatterplot(data=node_data, x="degree", y="energy", hue="Network")
+    #scatter.legend(fontsize=6)
+    plt.axvline(0, color='grey', linestyle='--', linewidth=1)
+
+    # add trendline for whole thing
+
+    X_plot = node_data["dC"]
+    Y_plot = node_data["energy"]
+    m, b = np.polyfit(X_plot, Y_plot, 1)
+    plt.plot(X_plot, m*X_plot + b, color='r')
+
+    plt.ylabel('% energy yield')
+
+    sns.despine()
+    scatter.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., fontsize=12)
+    plt.tight_layout()
+    plt.savefig('dc_vs_percent_energy.pdf')
+    #plt.savefig('degree_vs_energy.pdf')
+    plt.show()
+    plt.clf()
+
+    X_plot = node_data["dC"]
+    Y_plot = node_data["energy"]
+    X2 = sm.add_constant(X_plot)
+    est = sm.OLS(Y_plot, X2)
+    est2 = est.fit()
+    print("summary()\n", est2.summary())
+    print("pvalues\n", est2.pvalues)
+    print("f_pvalues\n", est2.f_pvalue)
+    print("tvalues\n", est2.tvalues)
+    print("rsquared\n", est2.rsquared)
+    print("rsquared_adj\n", est2.rsquared_adj)
+
+    # visualize networks
+
     for plume_network in plume_networks.keys():
 
         G = plume_networks[plume_network]
@@ -261,17 +486,17 @@ if __name__ == '__main__':
         node_attributes = dict(G.nodes.data())
         nodes = list(G.nodes())
         node_sizes = []
-        scale = 700
+        scale = 10
         for node in nodes:
             try:
                 node_sizes.append(scale*node_attributes[node]['energy'])
             except:
-                node_sizes.append(500)
+                node_sizes.append(10)
 
         # scaled nodes
         #nx.draw(G, pos=pos, with_labels=True, node_color=color_list, font_size=8, node_size=node_sizes)
-        nx.draw(G, pos=pos, with_labels=True, node_color=color_list, font_size=8)
-        #plt.show()
+        nx.draw(G, pos=pos, with_labels=True, node_color=color_list, font_size=8, node_size=node_sizes)
+        plt.show()
         plt.savefig(plume_network + '.pdf')
         plt.clf()
 
